543c70d9a78e75e414d6aaeece4982066e1d1d20
jnwync <jonwayne.cabusbusan@gmail.com>
2024-04-09 18:13:34 +0800

    still buggy

--- src/classes/board.ts
+++ src/classes/board.ts
@@ -1,89 +1,133 @@
 class Board {
-    private tiles: number[][]; // Private member to store the tiles
-    private n: number; // Size of the board
+  private readonly tiles: number[][];
+  private readonly n: number;
 
-    constructor(tiles: number[][]) {
-        this.tiles = tiles;
-        this.n = tiles.length;
+  constructor(tiles: number[][]) {
+    this.tiles = tiles;
+    this.n = tiles.length;
+  }
+
+  toString(): string {
+    let str = `${this.n}\n`;
+    for (let row = 0; row < this.n; row++) {
+      for (let col = 0; col < this.n; col++) {
+        str += `${this.tiles[row][col]} `;
+      }
+      str += "\n";
     }
+    return str;
+  }
 
-    toString(): string {
-        let str = `${this.n}\n`; // First line contains the board size
-        for (let row of this.tiles) {
-            str += row.join(" ") + "\n";
+  dimension(): number {
+    return this.n;
+  }
+
+  hamming(): number {
+    let count = 0;
+    for (let row = 0; row < this.n; row++) {
+      for (let col = 0; col < this.n; col++) {
+        if (
+          this.tiles[row][col] !== 0 &&
+          this.tiles[row][col] !== row * this.n + col + 1
+        ) {
+          count++;
         }
-        return str;
+      }
     }
+    return count;
+  }
 
-    dimension(): number {
-        return this.n;
-    }
-
-    hamming(): number {
-        let count = 0;
-        for (let i = 0; i < this.n; i++) {
-            for (let j = 0; j < this.n; j++) {
-                if (this.tiles[i][j] !== 0 && this.tiles[i][j] !== i * this.n + j + 1) {
-                    count++;
-                }
-            }
+  manhattan(): number {
+    let distance = 0;
+    for (let row = 0; row < this.n; row++) {
+      for (let col = 0; col < this.n; col++) {
+        const value = this.tiles[row][col];
+        if (value !== 0) {
+          const targetRow = Math.floor((value - 1) / this.n);
+          const targetCol = (value - 1) % this.n;
+          distance += Math.abs(row - targetRow) + Math.abs(col - targetCol);
         }
-        return count;
+      }
     }
+    return distance;
+  }
 
-    manhattan(): number {
-        let distance = 0;
-        for (let i = 0; i < this.n; i++) {
-            for (let j = 0; j < this.n; j++) {
-                const value = this.tiles[i][j];
-                if (value !== 0) {
-                    const goalX = Math.floor((value - 1) / this.n);
-                    const goalY = (value - 1) % this.n;
-                    distance += Math.abs(i - goalX) + Math.abs(j - goalY);
-                }
-            }
+  isGoal(): boolean {
+    return this.hamming() === 0;
+  }
+
+  equals(y: Board): boolean {
+    if (this.dimension() !== y.dimension()) return false;
+    for (let row = 0; row < this.n; row++) {
+      for (let col = 0; col < this.n; col++) {
+        if (this.tiles[row][col] !== y.tiles[row][col]) {
+          return false;
         }
-        return distance;
+      }
+    }
+    return true;
+  }
+
+  neighbors(): Board[] {
+    const blankPosition = this.findBlankPosition();
+    const neighbors = [];
+
+    const directions = [
+      [-1, 0],
+      [1, 0],
+      [0, -1],
+      [0, 1],
+    ]; // Up, Down, Left, Right
+
+    for (const [dx, dy] of directions) {
+      const newRow = blankPosition[0] + dx;
+      const newCol = blankPosition[1] + dy;
+      if (this.isValidPosition(newRow, newCol)) {
+        const neighborTiles = this.cloneTiles();
+        neighborTiles[blankPosition[0]][blankPosition[1]] =
+          neighborTiles[newRow][newCol];
+        neighborTiles[newRow][newCol] = 0;
+        neighbors.push(new Board(neighborTiles));
+      }
     }
 
-    isGoal(): boolean {
-        return this.hamming() === 0;
-    }
+    return neighbors;
+  }
 
-    equals(y: Board): boolean {
-        return this.toString() === y.toString();
-    }
-
-    neighbors(): Board[] {
-        const neighbors: Board[] = [];
-        const zeroPosition = this.findZeroPosition();
-        const directions = [[-1, 0], [1, 0], [0, -1], [0, 1]];
-        for (let dir of directions) {
-            const newX = zeroPosition[0] + dir[0];
-            const newY = zeroPosition[1] + dir[1];
-            if (newX >= 0 && newX < this.n && newY >= 0 && newY < this.n) {
-                const neighborTiles = this.cloneTiles();
-                [neighborTiles[zeroPosition[0]][zeroPosition[1]], neighborTiles[newX][newY]] = [neighborTiles[newX][newY], neighborTiles[zeroPosition[0]][zeroPosition[1]]];
-                neighbors.push(new Board(neighborTiles));
-            }
+  twin(): Board {
+    let twinTiles = this.cloneTiles();
+    for (let row = 0; row < this.n; row++) {
+      for (let col = 0; col < this.n - 1; col++) {
+        if (twinTiles[row][col] !== 0 && twinTiles[row][col + 1] !== 0) {
+          //LOOP BAYLO TWIN BOARD
+          const temp = twinTiles[row][col];
+          twinTiles[row][col] = twinTiles[row][col + 1];
+          twinTiles[row][col + 1] = temp;
+          return new Board(twinTiles);
         }
-        return neighbors;
+      }
     }
+    return new Board(twinTiles);
+  }
 
-    private findZeroPosition(): number[] {
-        for (let i = 0; i < this.n; i++) {
-            for (let j = 0; j < this.n; j++) {
-                if (this.tiles[i][j] === 0) {
-                    return [i, j];
-                }
-            }
+  private findBlankPosition(): [number, number] {
+    for (let row = 0; row < this.n; row++) {
+      for (let col = 0; col < this.n; col++) {
+        if (this.tiles[row][col] === 0) {
+          return [row, col];
         }
-        return [-1, -1];
+      }
     }
+    return [-1, -1]; //KUNG BLANK
+  }
 
-    private cloneTiles(): number[][] {
-        return this.tiles.map(row => [...row]);
-    }
+  private isValidPosition(row: number, col: number): boolean {
+    return row >= 0 && col >= 0 && row < this.n && col < this.n;
+  }
+
+  private cloneTiles(): number[][] {
+    return this.tiles.map((row) => [...row]);
+  }
 }
 
 export default Board;
--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,34 @@
-import Solver from "./classes/solver";
 import Board from "./classes/board";
+import Solver from "./classes/solver";
 import { readFileSync } from "fs";
 
 const fileName: string = process.argv[2];
 
 const lines: string[] = readFileSync(`./puzzles/${fileName}`, "utf8").split(
-    "\n"
+  "\n"
 );
 const n: number = parseInt(lines[0]);
-const tiles: number[][] = Array(n).fill(Array(n));
+const tiles: number[][] = [];
 
 lines.forEach((line, row) => {
-    if (row === 0) {
-        return;
-    }
+  if (row === 0) {
+    return;
+  }
 
-    const nums = line
-        .split(" ")
-        .map((s) => parseInt(s))
-        .filter((x) => !isNaN(x));
+  const nums = line
+    .split(" ")
+    .map((s) => parseInt(s))
+    .filter((x) => !isNaN(x));
 
-    if (nums.length === 0) {
-        return;
-    }
+  if (nums.length === 0) {
+    return;
+  }
 
-    tiles[row - 1] = nums;
+  tiles.push(nums);
 });
 
 console.log(tiles);
 
-// const initial: Board = new Board(tiles);
+const initialBoard = new Board(tiles);
 
-// // solve the puzzle
-// const solver: Solver = new Solver(initial);
-
-// // print solution to standard output
-// if (!solver.isSolvable()) {
-//     console.log("No solution possible");
-// } else {
-//     console.log("Minimum number of moves = " + solver.moves());
-//     for (let board of solver.solution()) {
-//         console.log(board.toString());
-//     }
-// }
+const solver = new Solver(initialBoard);
